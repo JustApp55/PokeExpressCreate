af3a6a9854cedd5db152935c42790ffc2a17064d
Justice A. Acheampong <iroyal@Justices-MBP.attlocal.net>
2022-03-09 14:36:00 -0500

    created form to create new pokemon

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,6 +1,7 @@
+require('dotenv').config({path: '.env'})
 // Import the express library here
 const express = require('express')
-const pokemon = require('./models/pokemon.js')
+const Pokemon = require('./models/pokemon.js')
 // Instantiate the app here
 const app = express()
 const port=3000
@@ -18,9 +19,25 @@ app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'jsx');
   app.engine('jsx', require('express-react-views').createEngine());
 
-app.get('/Index', (req, res)=>{
-    pokemon.find({}, (error, allPokemon)=>
-    res.render('Index', {pokemon: allPokemon}))
+  app.get('/pokemon/seed', (req, res)=>{
+    Pokemon.create([
+        {name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur"},
+        {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
+        {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
+        {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander"},
+        {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard"},
+        {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
+        {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"}
+     ], (err, data)=>{
+        res.redirect('/pokemon');
+    })
+});
+
+
+app.get('/pokemon', (req, res)=>{
+    Pokemon.find({}, (error, allPokemon)=> {
+    res.render('Index', {pokemon: allPokemon})})
+    
 })
 
 app.get('/pokemon/new', (req, res)=>{
